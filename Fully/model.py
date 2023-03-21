da8cade1c9fc69de2c948840ad6a20e470907508
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2023-03-21 20:55:48 +0330

    Implement Fully - a BiEncoder-like architecture that replaces cos_sim by a fully-connected layer

---
+++ Fully/model.py
@@ -0,0 +1,134 @@
+from typing import List, Union
+import torch
+from torch import nn
+from transformers import AutoModel, AutoTokenizer, LukeModel, LukeTokenizer
+
+
+class BiEncoder(nn.Module):
+    def __init__(self, model_name: str = None, output_size: int = 128, max_length: int = 512, device: str = None, ):
+        super().__init__()
+        self.device = device
+        self.max_length = max_length
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.text_language_model = AutoModel.from_pretrained(model_name)
+        # self.entity_language_model = LukeModel.from_pretrained('studio-ousia/luke-base')
+        self.query_layer = nn.Linear(768, output_size)
+        self.passage_layer = nn.Linear(768, output_size)
+        self.join_layer = nn.Linear(output_size * 2, 1)
+
+        if device is None:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+        self._target_device = torch.device(device)
+
+    def forward(self, queries, passages, phase):
+        queries_outputs = self.text_language_model(**queries)
+        queries_representation = queries_outputs.pooler_output
+        passages_outputs = self.text_language_model(**passages)
+        passages_representation = passages_outputs.pooler_output
+
+        queries_x = self.query_layer(queries_representation)
+        queries_x = torch.relu(queries_x)
+        passages_x = self.passage_layer(passages_representation)
+        passages_x = torch.relu(passages_x)
+        output = self.join_layer(torch.cat((queries_x, passages_x), dim=1))
+        output = torch.sigmoid(output)
+
+        return output
+
+    def batching_collate(self, batch):
+        texts = [[] for _ in range(len(batch[0].texts))]
+        entity_spans = [[] for _ in range(len(batch[0].entity_spans))]
+        entities = [[] for _ in range(len(batch[0].entities))]
+        labels = []
+
+        for example in batch:
+            for idx, text in enumerate(example.texts):
+                texts[idx].append(text.strip())
+
+            for idx, each_entity_spans in enumerate(example.entity_spans):
+                entity_spans[idx].append(each_entity_spans)
+
+            for idx, each_entities in enumerate(example.entities):
+                entities[idx].append(each_entities)
+
+            labels.append(example.label)
+
+        try:
+            queries_tokenized = self.tokenizer(texts[0], entity_spans=entity_spans[0], entities=entities[0],
+                                               padding=True, truncation='longest_first', return_tensors="pt",
+                                               max_length=self.max_length)
+            passages_tokenized = self.tokenizer(texts[1], entity_spans=entity_spans[1], entities=entities[1],
+                                                padding=True, truncation='longest_first', return_tensors="pt",
+                                                max_length=self.max_length)
+        except:
+            queries_tokenized = self.tokenizer(texts[0], padding=True, truncation='longest_first', return_tensors="pt",
+                                               max_length=self.max_length)
+            passages_tokenized = self.tokenizer(texts[1], padding=True, truncation='longest_first', return_tensors="pt",
+                                                max_length=self.max_length)
+
+        for name in queries_tokenized:
+            queries_tokenized[name] = queries_tokenized[name].to(self._target_device)
+        for name in passages_tokenized:
+            passages_tokenized[name] = passages_tokenized[name].to(self._target_device)
+        labels = torch.tensor(labels, dtype=torch.float).to(self._target_device)
+
+        return queries_tokenized, passages_tokenized, labels
+
+    def batching_collate_without_label(self, batch):
+        texts = [[], []]
+        entity_spans = [[], []]
+        entities = [[], []]
+
+        for example in batch:
+            for idx, text in enumerate(example[0]):
+                texts[idx].append(text.strip())
+
+            for idx, each_entity_spans in enumerate(example[1]):
+                entity_spans[idx].append(each_entity_spans)
+
+            for idx, each_entities in enumerate(example[2]):
+                entities[idx].append(each_entities)
+
+        try:
+            tokenized = self.tokenizer(*texts, *entity_spans, *entities, padding=True, truncation='longest_first',
+                                       return_tensors="pt", max_length=self.max_length)
+        except:
+            entity_spans = [[], []]
+            entities = [[], []]
+            tokenized = self.tokenizer(*texts, *entity_spans, *entities, padding=True, truncation='longest_first',
+                                       return_tensors="pt", max_length=self.max_length)
+            # tokenized = self.tokenizer(*texts, padding=True, truncation='longest_first', return_tensors="pt", max_length=self.max_length)
+
+        for name in tokenized:
+            tokenized[name] = tokenized[name].to(self._target_device)
+
+        return tokenized
+
+    def fit(self):
+        return None
+
+
+class InputExample:
+    """
+    Structure for one input example with texts, the label and a unique id
+    """
+
+    def __init__(self, guid: str = '', texts: List[str] = None, label: Union[int, float] = 0,
+                 entity_spans: List = [], entities: List = []):
+        """
+        Creates one InputExample with the given texts, guid, label, entity_spans, entities
+        :param guid
+            id for the example
+        :param texts
+            the texts for the example.
+        :param label
+            the label for the example
+        """
+        self.guid = guid
+        self.texts = texts
+        self.label = label
+        self.entity_spans = entity_spans
+        self.entities = entities
+
+    def __str__(self):
+        return "<InputExample> label: {}, texts: {}".format(str(self.label), "; ".join(self.texts))
---
+++ Fully/test_with_entities.py
@@ -0,0 +1,115 @@
+# from sentence_transformers import CrossEncoder
+from model import CrossEncoder
+import os
+from tqdm import tqdm
+import sys
+
+LOCAL = True if sys.platform == 'win32' else False
+# First, we define the transformer model we want to fine-tune
+# model_name = 'distilroberta-base'
+model_name = "studio-ousia/luke-base"
+model_save_path = 'output/cross-encoder_' + model_name.split("/")[-1] + '_with-entities' + '-latest'
+run_output_path = model_save_path + '/Run.txt'
+device = 'cpu' if LOCAL else 'cuda:1'
+
+model = CrossEncoder(model_save_path, device=device, num_labels=1)
+print(f'{model_save_path} model loaded.')
+
+### Now we read the MS Marco dataset
+if LOCAL:
+    data_folder = r'C:\Users\sajad\PycharmProjects\chameleon_entity_linking\msmarco'
+else:
+    data_folder = '/home/sajadeb/msmarco'
+
+#### Read the corpus files, that contain all the passages. Store them in the corpus dict
+print('Loading collection...')
+collection = {}
+collection_filepath = os.path.join(data_folder, 'collection.tsv')
+with open(collection_filepath, 'r', encoding='utf8') as f:
+    for line in f:
+        pid, passage = line.strip().split("\t")
+        collection[pid] = passage.strip()
+
+### Read the test queries, store in queries dict
+print('Loading queries...')
+queries = {}
+queries_filepath = os.path.join(data_folder, 'queries.dev.small.tsv')
+with open(queries_filepath, 'r', encoding='utf8') as f:
+    for line in f:
+        qid, query = line.strip().split("\t")
+        queries[qid] = query.strip()
+
+### Read the train passages entities, store in passages_entities dict
+passages_entities = {}
+passages_entities_filepath = os.path.join(data_folder, 'entities', 'docs_entities.tsv')
+with open(passages_entities_filepath, 'r', encoding='utf8') as fIn:
+    print('Loading passages entities...')
+    for line in fIn:
+        pid, entities = line.strip().split("\t")
+        passages_entities[pid] = eval(entities)
+
+### Read the train queries entities, store in queries_entities dict
+queries_entities = {}
+queries_entities_filepath = os.path.join(data_folder, 'entities', 'dev_small_queries_entities.tsv')
+with open(queries_entities_filepath, 'r', encoding='utf8') as fIn:
+    print('Loading queries entities...')
+    for line in fIn:
+        qid, entities = line.strip().split("\t")
+        queries_entities[qid] = eval(entities)
+
+
+print('Loading qrels...')
+qrels = {}
+if LOCAL:
+    qrels_filepath = os.path.join(data_folder, 'runbm25anserini.dev')
+else:
+    qrels_filepath = os.path.join(data_folder, 'runbm25anserini_notnull.dev')
+with open(qrels_filepath, 'r', encoding='utf8') as f:
+    for line in f:
+        qrel = line.strip().split(" ")
+        qid = qrel[0]
+        pid = qrel[2]
+        if qid in qrels:
+            qrels[qid].append(pid)
+        else:
+            qrels[qid] = [pid]
+
+# Search in a loop for the individual queries
+ranks = {}
+cnt = 0
+for qid, passages in tqdm(qrels.items()):
+    query = queries[qid]
+    query_entity_spans = [(entity['start'], entity['end']) for entity in queries_entities[qid]]
+    query_entities = [entity.get('title', 'spot') for entity in queries_entities[qid]]
+
+    collection_entity_spans = {}
+    collection_entities = {}
+    for pid in passages:
+        if pid in collection_entity_spans:
+            collection_entity_spans[pid].append([(entity['start'], entity['end']) for entity in passages_entities[pid]])
+        else:
+            collection_entity_spans[pid] = [(entity['start'], entity['end']) for entity in passages_entities[pid]]
+        if pid in collection_entities:
+            collection_entities[pid].append([entity.get('title', 'spot') for entity in passages_entities[pid]])
+        else:
+            collection_entities[pid] = [entity.get('title', 'spot') for entity in passages_entities[pid]]
+
+    # Concatenate the query and all passages and predict the scores for the pairs [query, passage]
+    model_inputs = [[[query, collection[pid]], [query_entity_spans, collection_entity_spans[pid]], [query_entities, collection_entities[pid]]] for pid in passages]
+    scores = model.predict(model_inputs)
+
+    # Sort the scores in decreasing order
+    results = [{'pid': pid, 'score': score} for pid, score in zip(passages, scores)]
+    results = sorted(results, key=lambda x: x['score'], reverse=True)
+
+    ranks[qid] = results
+
+print('Writing the result to file...')
+with open(run_output_path, 'w', encoding='utf-8') as out:
+    for qid, results in ranks.items():
+        rank = 1
+        for hit in results:
+            out.write(
+                str(qid) + ' Q0 ' + hit['pid'] + ' ' + str(rank) + ' ' + str(hit['score']) + ' ' + model_name.replace(
+                    "/", "-") + '\n')
+            rank = rank + 1
