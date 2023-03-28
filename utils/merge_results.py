35e4f9f4cce57c6b2c2ff5d4f709d92ca844ced8
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2023-03-28 19:05:23 +0430

    Update merge_results to use all-retrieved file as the base

--- utils/merge_results.py
+++ utils/merge_results.py
@@ -1,18 +1,21 @@
 from tqdm import tqdm
 
 results = {}
-with open('Run.txt', 'r') as f, open('Run_null.txt', 'r') as n:
-    for line in tqdm(f):
+pruned_run_file = '/home/sajadeb/chameleon_entity_linking/SBERT/output/cross-encoder_luke-base_with-entities-entities-latest/Run.txt'
+entire_run_file = '/home/sajadeb/chameleon_entity_linking/SBERT/output/cross-encoder_luke-base_with-entities-entities-latest/Run_full.txt'
+with open(pruned_run_file, 'r') as pruned, open(entire_run_file, 'r') as entire:
+    for line in tqdm(pruned):
         data = line.strip().split()
         if data[0] in results:
             results[data[0]].append({'pid': data[2], 'score': data[4]})
         else:
             results[data[0]] = [{'pid': data[2], 'score': data[4]}]
 
-    for line in tqdm(n):
+    for line in tqdm(entire):
         data = line.strip().split()
         if data[0] in results:
-            results[data[0]].append({'pid': data[2], 'score': data[4]})
+            if data[2] not in [hit['pid'] for hit in results[data[0]]]:
+                results[data[0]].append({'pid': data[2], 'score': data[4]})
         else:
             results[data[0]] = [{'pid': data[2], 'score': data[4]}]
 
@@ -21,5 +24,5 @@ with open('Run_merged.txt', 'w', encoding='utf-8') as out:
         passages = sorted(results[qid], key=lambda x: x['score'], reverse=True)
         rank = 1
         for hit in passages:
-            out.write(str(qid) + ' Q0 ' + hit['pid'] + ' ' + str(rank) + ' ' + str(hit['score']) + ' ' + 'OA_on_all' + '\n')
+            out.write(str(qid) + ' Q0 ' + hit['pid'] + ' ' + str(rank) + ' ' + str(hit['score']) + ' ' + 'merged_with_all' + '\n')
             rank = rank + 1
